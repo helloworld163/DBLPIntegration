c5d058e061764d7e7e32ad55558abd4dc27722aa
Susen Zhao <prcstyler@gmail.com>
2012-12-06 01:58:48 -0800

    multi-threaded caching of several authors for demo

--- src/authorcaching.py
+++ src/authorcaching.py
@@ -1,1 +1,32 @@
-__author__ = 'susen'
+# encoding: utf-8
+
+import threading
+import datetime
+import json
+from CDBLPAuthor import CDBLPAuthor
+from time import sleep
+
+class AuthorThread(threading.Thread):
+
+    authors = {}
+
+    def __init__(self, author_name, id):
+        threading.Thread.__init__(self)
+        self.author_name = author_name
+        self.id = id
+
+    def run(self):
+        author = CDBLPAuthor(self.author_name.strip())
+        #AuthorThread.authors[self.author_name.strip()] = author.get_author()
+        chunk = open('authors/{}-{}-entry.data'.format(self.id, self.author_name.strip()), 'w')
+        chunk.write(json.dumps(author.get_author()))
+        chunk.close()
+        print(self.author_name + ' %d is done.' % self.id)
+
+author_list = json.loads(open('author-cache.data', 'r').read())
+
+id = 0
+for author in author_list:
+    t = AuthorThread(author['zh'], id)
+    t.start()
+    id += 1
--- src/combinechunks.py
+++ src/combinechunks.py
@@ -1,1 +1,24 @@
-__author__ = 'susen'
+# encoding: utf-8
+
+import json
+from CDBLPAuthor import CDBLPAuthor
+
+author_list = json.loads(open('author-cache.data', 'r').read())
+
+authors = {}
+
+id = 0
+for author in author_list:
+    author_name = author['zh'].strip()
+    chunk = open('authors/{}-{}-entry.data'.format(id, author_name), 'r')
+    try:
+        authors[author_name] = json.loads(chunk.read())
+    except ValueError:
+        print(author_name)
+
+    chunk.close()
+    id += 1
+
+cache = open('author-entries-cache.data', 'w')
+cache.write(json.dumps(authors))
+cache.close()
